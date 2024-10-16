3762cc415bb7ef15830dafef7d0991e60d4263b0
Woomy <maria.dombrowska2007@gmail.com>
2024-10-16 13:44:32 +0200

    wip

--- API/RegisterFormHandler.js
+++ API/RegisterFormHandler.js
@@ -2,3 +2,4 @@ const sqlite3 = require("sqlite3").verbose()
 const path = require("path");
 
 const DbPath = path.join(__dirname, "../Data/StudietidDB.sqlite")
+
--- public/app.js
+++ public/app.js
@@ -5,3 +5,28 @@ document.querySelector('.home-btn').addEventListener('click', function() {
 document.querySelector('.add-btn').addEventListener('click', function() {
     window.location.href = '/register';
 });
+
+const registerBtn = document.getElementById("post_");
+const dateEl = document.querySelector("input[name='Date']");
+const timeOneEl = document.getElementById("timeOne");
+const timeTwoEl = document.getElementById("timeTwo");
+const roomSelectEl = document.getElementById("rooms");
+const subjectEl = document.querySelector("input[name='Fag']");
+const teacherEl = document.getElementById("lærer");
+const målEl = document.querySelector("textarea[name='Mål']")
+
+function calculateTime() {
+    let totalHours = 0;
+    let timeRange = "";
+
+    if (timeOne.checked && timeTwo.checked) {
+        totalHours = 2; // Both time slots selected
+        timeRange = "15:00 - 17:00";
+    } else if (timeOne.checked) {
+        totalHours = 1; // First time slot selected
+        timeRange = "15:00 - 16:00";
+    } else if (timeTwo.checked) {
+        totalHours = 1; // Second time slot selected
+        timeRange = "16:00 - 17:00";
+    }
+};
